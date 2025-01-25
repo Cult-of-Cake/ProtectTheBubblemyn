5737f7d2084641525c4fdbbe091af0f06bf42236
Laura <parmeisan@gmail.com>
2025-01-25 11:47:37 -0600

    Enemies spawning on a timer

--- godot/code/Enemy.gd
+++ godot/code/Enemy.gd
@@ -1,4 +1,4 @@
-extends Node
+extends CharacterBody2D
 class_name Enemy
 
 enum pathOptions { STAND, WANDER, MOVE_TO_PLAYER }
@@ -7,15 +7,24 @@ enum pathOptions { STAND, WANDER, MOVE_TO_PLAYER }
 @export var speed : float
 @export var path : pathOptions
 
+var goal_position : Vector2
 func set_goal_point() -> void:
-	print("global_position")
+	match path:
+		pathOptions.WANDER:
+			const magnitude = 100
+			randomize()
+			var x_variation = (randi() % 2*magnitude) - magnitude
+			var y_variation = (randi() % 2*magnitude) - magnitude
+			goal_position += Vector2(x_variation, y_variation)
+			print(global_position)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if path != pathOptions.STAND:
 		$MovementTimer.start()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var direction = global_position.direction_to(goal_position)
+	velocity = direction * speed
+	move_and_slide()
