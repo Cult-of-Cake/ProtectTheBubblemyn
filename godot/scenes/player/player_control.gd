4fba61c672132d0716807bd1de917d8b62b1b29e
Some Guy <hoodedperson1547@gmail.com>
2025-01-24 22:58:44 -0600

    Basic Player script
    Added player movement and take in attack inputs (Mouse click and space).

---
+++ godot/scenes/player/player_control.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const ACCEL = 20.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	
+	 # 8 Direction movement inputs
+	var direction := Input.get_vector("move_left", "move_right","move_up", "move_down")
+	
+	# This will have the player deaccelerate
+	velocity = lerp(velocity, direction * SPEED, ACCEL * delta) 
+	
+	# This moves the character
+	move_and_slide()
+	
+	_attack()
+
+func _attack():
+	
+	# Recieves input to attack
+	if Input.is_action_just_pressed("attack"):
+		
+		# Add what the attack does here
+		print("attack occured")
