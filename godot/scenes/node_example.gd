4f4a7e0819b36e2e4d42c5cc40b3275c17bc9b63
Tiernan Hubble <thubble@iqmetrix.com>
2025-01-25 12:40:40 -0600

    Add basic bubble shooter weapon and instantiate on the player.

--- godot/scenes/node_example.gd
+++ godot/scenes/node_example.gd
@@ -1,5 +1,16 @@
 extends Sprite2D
 
+const basic_bubble_shooter_template = preload("res://scenes/game/basic_bubble_shooter.tscn")
+
+func _ready() -> void:
+	var firstWeapon = basic_bubble_shooter_template.instantiate()
+	add_child(firstWeapon)
+	firstWeapon.global_position = Vector2(50, 0)
+	
+	var secondWeapon = basic_bubble_shooter_template.instantiate()
+	add_child(secondWeapon)
+	firstWeapon.global_position = Vector2(-50, 0)
+
 # specify in nodes to load data
 # from save game for this node
 func load_data(data:Dictionary) -> void:
