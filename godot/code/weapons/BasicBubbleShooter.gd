1f66f5c2d1440a947b98703e81feb6274ac80a8d
Tiernan Hubble <thubble@iqmetrix.com>
2025-01-25 13:59:59 -0600

    Bubble projectile hookup.

--- godot/code/weapons/BasicBubbleShooter.gd
+++ godot/code/weapons/BasicBubbleShooter.gd
@@ -1,6 +1,8 @@
 extends Sprite2D
 class_name BasicBubbleShooter
 
+const bubble_projectile_template = preload("res://scenes/game/projectiles/bubble_projectile.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +11,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+
+func shoot() -> void:
 	pass
