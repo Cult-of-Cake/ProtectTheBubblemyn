44f31f972742defaff448fb137f73d74c4fc03d1
Laura <parmeisan@gmail.com>
2025-01-25 13:02:50 -0600

    Fixed "wander" algorithm

--- godot/code/Enemy.gd
+++ godot/code/Enemy.gd
@@ -13,10 +13,10 @@ func set_goal_point() -> void:
 		pathOptions.WANDER:
 			const magnitude = 100
 			randomize()
-			var x_variation = (randi() % 2*magnitude) - magnitude
-			var y_variation = (randi() % 2*magnitude) - magnitude
-			goal_position += Vector2(x_variation, y_variation)
-			print(global_position)
+			var x_variation = randi_range(-magnitude, magnitude)
+			var y_variation = randi_range(-magnitude, magnitude)
+			goal_position = global_position + Vector2(x_variation, y_variation)
+			$MovementTimer.wait_time = randf_range(1, 2.5)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,5 +26,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var direction = global_position.direction_to(goal_position)
-	velocity = direction * speed
+	velocity = direction * speed * 5
 	move_and_slide()
