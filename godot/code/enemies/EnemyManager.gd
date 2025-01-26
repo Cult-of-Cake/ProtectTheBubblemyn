3218e5ba0fb3988f4292721912dfe2010159cdf1
PaulGGJ <paulsmart525@gmail.com>
2025-01-26 12:43:45 -0600

    Clodmucker initial
    Added clodmucker, changed toxidrop to seeker

--- godot/code/enemies/EnemyManager.gd
+++ godot/code/enemies/EnemyManager.gd
@@ -10,6 +10,7 @@ var enemy_template = preload("res://scenes/game/enemies/enemy_template.tscn")
 
 var enemy_splugey = preload("res://scenes/game/enemies/splugey.tscn")
 var enemy_salmonillo = preload("res://scenes/game/enemies/Salmonillo.tscn")
+var enemy_clodmucker = preload("res://scenes/game/enemies/Clodmucker.tscn")
 
 var intensity = 0
 
@@ -17,6 +18,7 @@ var temp_rate = 0
 
 var splug_rate = 0
 var salm_rate = 0
+var clod_rate = 0
 
 
 var kill_count = [0, 0]
@@ -41,7 +43,11 @@ func on_spawn_timer() -> void:
 		if salm_rate >= 5:
 			enemySpawn(enemy_salmonillo)
 			salm_rate = 0
-			
+		
+		clod_rate += 1
+		if clod_rate >= 5:
+			enemySpawn(enemy_clodmucker)
+			clod_rate = 0
 	
 func on_intensity_timer() -> void:
 	intensity += 1
