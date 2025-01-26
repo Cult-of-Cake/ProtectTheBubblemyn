da3a31551a32be9f4e87ab3c47e33fabe12fecf9
PaulGGJ <paulsmart525@gmail.com>
2025-01-26 10:43:47 -0600

    Arranged and set sprites
    Added sprites for dust fluffy and toxidrop

--- godot/code/enemies/Enemy.gd
+++ godot/code/enemies/Enemy.gd
@@ -27,6 +27,8 @@ func set_goal_point() -> void:
 			$MovementTimer.wait_time = randf_range(1, 2.5)
 		pathOptions.MOVE_TO_PLAYER:
 			goal_position = get_parent().get_parent().get_node("Player").get_node("Player").position
+	var direction = get_direction()
+	choose_sprite(direction)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if path != pathOptions.STAND:
@@ -43,7 +45,40 @@ func take_damage(damage: int) -> void:
 	current_health -= damage
 	if current_health < 0:
 		die()
-	
+
+func get_direction():
+	var direction
+	if abs(velocity.x) > abs (velocity.y):
+		if velocity.x > 0: 
+			direction = "right"
+		elif velocity.x < 0:
+			direction = "left"
+	elif abs(velocity.y) > abs (velocity.x):
+		if velocity.y > 0:
+			direction = "down"
+		elif velocity.y < 0:
+			direction = "up"
+	else:
+		direction = "down"
+	return direction
+func choose_sprite(direction):
+	remove_sprites()
+	if(direction == "right"):
+		get_node("Right").visible = true
+	elif(direction == "left"):
+		get_node("Left").visible = true
+	elif(direction == "up"):
+		get_node("Away").visible = true
+	elif(direction == "down"):
+		get_node("Facing").visible = true
+
+func remove_sprites():
+	get_node("Facing").visible = false
+	get_node("Left").visible = false
+	get_node("Right").visible = false
+	get_node("Away").visible = false
+
+
 func die() -> void:
 	died.emit(self)
 	queue_free()
